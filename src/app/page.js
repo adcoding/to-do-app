c15f9fd14e1b9a8a7e7d8bfac4bd37b45378849d
Adele <adeledendena@gmail.com>
2023-11-03 17:14:10 +0100

    changed tasks behavior and style

--- src/app/page.js
+++ src/app/page.js
@@ -3,22 +3,33 @@ import React , { useState , useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
-
-
 export default function Home() {
 
+  
   // Load 'tasks' from local storage or initialize as an empty array
   const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
   const [tasks, setTasks] = useState(storedTasks)
   const [task, setTask] = useState('')
 
+  const storedCheckedState = JSON.parse(localStorage.getItem('checkedState'));
+
+  const [checkedState, setCheckedState] = useState(() => {
+    if (storedCheckedState && storedCheckedState.length === tasks.length) {
+      return storedCheckedState;
+    } else {
+      return tasks.map(() => false);
+    }
+  });
+
   const addTask = () => {
     if (task.trim() !== '') {
       const newTasks = [...tasks, task];
+      const newCheckedState = [...checkedState, false]
       setTasks(newTasks);
       setTask('')
       // Update local storage with the new tasks array
       localStorage.setItem('tasks', JSON.stringify(newTasks));
+      localStorage.setItem('checkedState', JSON.stringify(newCheckedState))
     }
   }
 
@@ -44,8 +55,22 @@ export default function Home() {
           <ul>
             {tasks.map((task, index) => (
               <li key={index}>
-                <input type="checkbox" id={`task-${index}`} />
-                <label htmlFor={`task-${index}`}>{task}</label>
+                <input 
+                type="checkbox" 
+                className="checkbox" 
+                id={`task-${index}`}
+                checked= {checkedState[index]}
+                onChange={() => {
+                  const newCheckedState = [...checkedState];
+                  newCheckedState[index] = !newCheckedState[index];
+                  setCheckedState(newCheckedState);
+                  localStorage.setItem('checkedState', JSON.stringify(newCheckedState)); // Update checked state in local storage
+                }}
+                />
+                <label className='checkmark' htmlFor={`task-${index}`}>{task}</label>
+
+
+
                 <button onClick={() => deleteTask(index)}>
                   <FontAwesomeIcon icon={faCircleXmark} size="lg" style={{ color: "#8a8a89" }} />
                 </button>
